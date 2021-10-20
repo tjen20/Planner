0898deca79f8e4f5751aad2adc65ed943edb4454
Teri Jensen <teriannjensen@gmail.com>
2021-10-20 10:42:04 -0600

    trying to get date/time

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 // Time and Date
 
+let today = new Date();
+let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let dateTime = date+' '+time;
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
+
 
 
 // Color coded for past, present, and future
