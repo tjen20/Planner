a37e4cbdc557a0b9dbb9cfd73030b09a829b52d8
Teri Jensen <teriannjensen@gmail.com>
2021-10-22 19:04:42 -0600

    fixed colors

--- assets/script.js
+++ assets/script.js
@@ -23,18 +23,128 @@ let inputEl = document.getElementById("description");
 
 // Color coded for past, present, and future
 
-let currentTime = $('.description');
+let currentTime = $('#description');
 
-    if (inputEl < actualTime) {
-        $(description).addClass('past')
+    if(moment().format('HH') > 8){
+        $('#8').addClass('past')
     }
 
-    else if (inputEl === actualTime) {
-        $(description).addClass('present')
+    if(moment().format('HH') > 9){
+        $('#9').addClass('past')
     }
 
-    else if (inputEl > actualTime) {
-        $(description).addClass('future')
+    if(moment().format('HH') > 10){
+        $('#10').addClass('past')
+    }
+
+    if(moment().format('HH') > 11){
+        $('#11').addClass('past')
+    }
+
+    if(moment().format('HH') > 12){
+        $('#12').addClass('past')
+    }
+
+    if(moment().format('HH') > 13){
+        $('#13').addClass('past')
+    }
+
+    if(moment().format('HH') > 14){
+        $('#14').addClass('past')
+    }
+
+    if(moment().format('HH') > 15){
+        $('#15').addClass('past')
+    }
+
+    if(moment().format('HH') > 16){
+        $('#16').addClass('past')
+    }
+
+    if(moment().format('HH') > 17){
+        $('#17').addClass('past')
+    }
+
+// for future
+    if(moment().format('HH') < 8){
+        $('#8').addClass('future')
+    }
+
+    if(moment().format('HH') < 9){
+        $('#9').addClass('future')
+    }
+
+    if(moment().format('HH') < 10){
+        $('#10').addClass('future')
+    }
+
+    if(moment().format('HH') < 11){
+        $('#11').addClass('future')
+    }
+
+    if(moment().format('HH') < 12){
+        $('#12').addClass('future')
+    }
+
+    if(moment().format('HH') < 13){
+        $('#13').addClass('future')
+    }
+
+    if(moment().format('HH') < 14){
+        $('#14').addClass('future')
+    }
+
+    if(moment().format('HH') < 15){
+        $('#15').addClass('future')
+    }
+
+    if(moment().format('HH') < 16){
+        $('#16').addClass('future')
+    }
+
+    if(moment().format('HH') < 17){
+        $('#17').addClass('future')
+    }
+
+    // for present
+    if(moment().format('HH') === 8){
+        $('#8').addClass('present')
+    }
+
+    if(moment().format('HH') === 9){
+        $('#9').addClass('present')
+    }
+
+    if(moment().format('HH') === 10){
+        $('#10').addClass('present')
+    }
+
+    if(moment().format('HH') === 11){
+        $('#11').addClass('present')
+    }
+
+    if(moment().format('HH') === 12){
+        $('#12').addClass('present')
+    }
+
+    if(moment().format('HH') === 13){
+        $('#13').addClass('present')
+    }
+
+    if(moment().format('HH') === 14){
+        $('#14').addClass('present')
+    }
+
+    if(moment().format('HH') === 15){
+        $('#15').addClass('present')
+    }
+
+    if(moment().format('HH') === 16){
+        $('#16').addClass('present')
+    }
+
+    if(moment().format('HH') === 17){
+        $('#17').addClass('present')
     }
 
 // Save to Local Storage
